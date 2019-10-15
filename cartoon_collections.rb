e9ea847e2a1e213baaec225ef3c406a51b2c2361
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-15 22:19:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index do |dwarf, index|
     array << "#{index}.#{dwarf}"
   end
-  puts array.join
+  p array.join
 end
 
 def summon_captain_planet# code an argument here
