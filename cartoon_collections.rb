de36be2605c44f6fd3826aae006e00a7e69b4387
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-15 22:24:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,8 @@ def roll_call_dwarves(dwarves)# code an argument here
 end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
-  # Your code here
+  planeteer_calls.map do |call|
+    
 end
 
 def long_planeteer_calls# code an argument here
