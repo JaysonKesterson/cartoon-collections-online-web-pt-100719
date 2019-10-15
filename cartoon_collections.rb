246e021e92ebad34761d8aca60e8b4e32a47bb29
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-15 22:17:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)# code an argument here
-  roll_call_dwarves.each_with_index do |dwarf, index|
+  dwarves.each_with_index do |dwarf, index|
     print "#{index}.#{dwarf}"
   end
 end
