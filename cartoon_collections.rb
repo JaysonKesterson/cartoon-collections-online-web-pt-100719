0215bd01f9b00c2f703eca926b7c0661cbc07040
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-15 22:32:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,18 @@
-def roll_call_dwarves(dwarves)# code an argument here
+def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     print "#{index + 1}.#{dwarf}"
   end
   
 end
 
-def summon_captain_planet(planeteer_calls)# code an argument here
+def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |call|
     call.capitalize + "!"
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  
 end
 
 def find_the_cheese# code an argument here
